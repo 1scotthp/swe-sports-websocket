c00a3386545f9b0e29e9a588e94ec40df0767826
1scotthp <51714937+1scotthp@users.noreply.github.com>
2022-03-22 14:59:46 -0500

    multiple group chats working locally

--- app/index.mjs
+++ app/index.mjs
@@ -3,18 +3,22 @@ import kafka from "kafkajs";
 import http from "http";
 import ws from "websocket";
 import redis from "redis";
-import { appendFile } from "fs";
+
 const APPID = process.env.APPID;
-let connections = [];
+let connections = {}; // connection dictionary indexed by gc id
 const WebSocketServer = ws.server;
 
 const port = process.env.PORT || 8080;
 
-let group_chat_dict = {}
+let connectionUidDict = {};
+let uidGroupChatDict = {};
 
 const onRecieveMessage = (message) => {
-  console.log("REC");
-  connections.forEach((c) => c.send(APPID + ":" + message));
+  console.log("REC", message, typeof message);
+
+  connections[JSON.parse(message)["group_chat"]].forEach((c) => {
+    c["con"].send(APPID + ":" + message);
+  });
 };
 
 //create a raw http server (this will help us create the TCP which will then pass to the websocket to do the job)
@@ -25,7 +29,6 @@ const websocket = new WebSocketServer({
   httpServer: httpserver,
 });
 
-
 httpserver.listen(port, () =>
   console.log("My server is listening on port " + port)
 );
@@ -33,27 +36,50 @@ httpserver.listen(port, () =>
 //when a legit websocket request comes listen to it and get the connection .. once you get a connection thats it!
 websocket.on("request", (request) => {
   const con = request.accept(null, request.origin);
+
   con.on("open", () => console.log("opened"));
-  con.on("close", () => console.log("CLOSED!!!"));
+  con.on("close", () => {
+    console.log("CLOSED!!!");
+    const uid = connectionUidDict[con];
+    const group_chat_list = uidGroupChatDict[uid];
+
+    for (let gc of group_chat_list) {
+      connections[gc] = connections[gc].filter(
+        (conObject) => conObject["uid"] != uid
+      );
+    }
+  });
   con.on("message", (message) => {
     console.log(`${APPID} Received message hi ${message.utf8Data}`);
 
     // if its an initialization message
-    console.log(message.utf8Data, typeof(message.utf8Data))
-    // if(Object.keys(message.utf8Data).includes("group_chats")){
-    //   console.log("initial connect")
-    // } else{ // its a normal message
-    //   onRecieveMessage(message.utf8Data);
-    // }
+    let messageObject = JSON.parse(message.utf8Data);
 
+    if (Object.keys(messageObject).includes("group_chats")) {
+      // remove connections
 
+      // add connections
+      console.log("adding");
+      uidGroupChatDict[messageObject["uid"]] = messageObject["group_chats"];
+      for (let gc of messageObject["group_chats"]) {
+        connectionUidDict[con] = messageObject["uid"];
+
+        if (connections[gc] == undefined) {
+          connections[gc] = [{ uid: messageObject["uid"], con: con }];
+        } else {
+          connections[gc].push({ uid: messageObject["uid"], con: con });
+          //group_chat_dict[gc] = group_chat_dict[gc].push(messageObject["uid"]);
+        }
+      }
+    } else {
+      // its a normal message
+
+      onRecieveMessage(message.utf8Data);
+    }
   });
 
-  setTimeout(() => con.send(`Connected successfully to server ${APPID}`), 5000);
-  connections.push(con);
+  // setTimeout(() => con.send(`Connected successfully to server ${APPID}`), 5000);
 });
-
-
 
 //client code
 //let ws = new WebSocket("ws://localhost:8080");
